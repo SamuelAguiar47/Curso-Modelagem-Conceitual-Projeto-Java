829358c3c17dd29aab2a6fd3eb1262a50dd470fd
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2025-06-17 10:28:04 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,7 @@
 package com.nelioalves.cursomc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.nelioalves.cursomc.domain.Categoria;
 
 @SpringBootApplication
 public class CursomcApplication {	
