f44bf4a524cb6dc1c84ef0416791e274c31f0c48
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2025-06-17 09:59:09 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,11 +1,16 @@
 package com.nelioalves.cursomc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nelioalves.cursomc.domain.Categoria;
 
+@SpringBootApplication
+public class CursomcApplication {	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
